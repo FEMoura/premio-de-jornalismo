26071a58b72d79d1d7c5f74ec7cd21a020483bcf
Nywton Barros <nywton.barros@gmail.com>
2016-09-23 12:53:22 -0300

    Navbar ease scroll anchor [done]

--- app/assets/javascripts/scroll-page.js
+++ app/assets/javascripts/scroll-page.js
@@ -1,13 +1,14 @@
-(function($) {
+
+$(document).ready(function() {
     "use strict"; // Start of use strict
 
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('.page-scroll').on('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 500);
+            scrollTop: ($($anchor.attr('href')).offset().top - 91)
+        }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
@@ -56,4 +57,4 @@
     //    }
     //});
 
-})(jQuery); // End of use strict
+});
