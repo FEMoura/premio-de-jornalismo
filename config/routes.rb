0d6c56e5a0205208118eb9272677287870b3b003
Ernande Junior <ernande.junior@gmail.com>
2016-10-05 13:48:35 -0300

    Opcao de recuperar senha, in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' }
   resources :users
   namespace :restrict do
   devise_scope :user do
