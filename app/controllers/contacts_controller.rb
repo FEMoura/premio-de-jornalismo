2b0ca3fb5c71e48f1a561fc4ecf681e65e2f2cce
marksack <marksack@vagrant-rails.secti.local>
2016-10-06 11:08:02 -0300

    Enable email for contact form - in progress - missing expresso user and password

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,13 +3,30 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      ContactMailer.contact_confirm_email(contact_params).deliver_later
+      ContactMailer.contact_request_received(contact_params).deliver_later
       redirect_to root_path(anchor: 'contact'), notice: 'Mensagem enviada com sucesso, em breve retornaremos seu contato.'
     else
       contact_hash = Hash.new
       contact_hash[:name] = @contact.name
       contact_hash[:email] = @contact.email
       contact_hash[:message] = @contact.message
-      redirect_to root_path(anchor: 'contact', contact: contact_hash), alert: 'Mensagem não enviada. Verificar os dados.'
+      error_notice = String.new
+      if @contact.name.blank?
+        error_notice << 'Nome não pode ficar em branco.'
+      end
+      if @contact.email.blank?
+        error_notice << ' Email não pode ficar em branco.'
+      end
+      if @contact.message.blank?
+        error_notice << ' Mensagem não pode ficar em branco.'
+      end
+      if @contact.name && @contact.email && @contact.message && error_notice.blank?
+        error_notice << ' Verificar o formato do email.'
+      end
+      redirect_to root_path(anchor: 'contact', contact: contact_hash),
+                  alert: 'Mensagem não enviada. Verificar os dados.',
+                  notice: error_notice
     end
   end
 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,15 @@
 class ContactMailer < ApplicationMailer
+  default from: Settings.email.contact_confirm_address
+
+  def contact_confirm_email(contact)
+    @contact = contact
+    attachments.inline['logo.png'] = File.read('public/images/logo.png')
+    mail(to: [contact[:email]], subject: "PREMIO DE JORNALISMO: Contato")
+  end
+
+  def contact_request_received(contact)
+    @contact = contact
+    subject_string = 'Contato Premio de Jornalismo de ' + contact[:name]
+    mail(to: Settings.email.contact_confirm_address, from: contact[:email], subject: subject_string)
+  end
 end
