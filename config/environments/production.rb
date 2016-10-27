8c4c81d967219226302d2026d9db1a85bbce9377
marksack <marksack@vagrant-rails.secti.local>
2016-10-27 14:29:52 -0300

    Update email account and password in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "mail.expresso.al.gov.br",
                                          :port => 587,
-                                         :user_name => 'xxxx',
-                                         :password => 'yyyy',
+                                         :user_name => 'contato.premiojornalismocientifico',
+                                         :password => 'premio953',
                                          :authentication => 'plain'
   }
 end
