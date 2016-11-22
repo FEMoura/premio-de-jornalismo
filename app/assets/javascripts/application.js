782fe1a11a1efcca8a4e31ff79254b2c150c5884
Ernande Junior <ernande.junior@gmail.com>
2016-11-22 15:17:18 -0300

    Poup-up Capacitacao

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,4 +43,26 @@ $(document).ready(function(){
             $scroll.css({ backgroundPosition: coords });
         });
     });
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+
 });
